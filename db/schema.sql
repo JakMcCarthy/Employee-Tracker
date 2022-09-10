521897d1b8a2c6c8eab4daa98173c5d6859aa67a
JakMcCarthy <90810973+JakMcCarthy@users.noreply.github.com>
2022-09-10 17:39:18 -0400

    Create employee table and corresponding seeds

--- db/schema.sql
+++ db/schema.sql
@@ -11,3 +11,15 @@ CREATE TABLE role (
     FOREIGN KEY (department_id) REFERENCES department(id) 
     ON DELETE SET NULL
 );
+
+CREATE TABLE employee (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    last_name VARCHAR(30),
+    first_name VARCHAR(30),
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES role(id) 
+    ON DELETE SET NULL,
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
+    ON DELETE SET NULL
+);
--- db/seeds.sql
+++ db/seeds.sql
@@ -15,3 +15,16 @@ VALUES
     ('Junior Attorney', 190000, '3'),
     ('Sales Lead', 100000, '4'),
     ('Client Specialist', 60000, '4');
+
+INSERT INTO employee (last_name, first_name, role_id, manager_id)
+VALUES
+    ('Tate', 'Jonathon', '1', null),
+    ('Hobbs', 'Janiyah', '3', null),
+    ('Edwards', 'Heidi', '5', null),
+    ('Perkins', 'Cecelia', '7', null),
+    ('Odom', 'Luka', '2', 1),
+    ('McMahon', 'Gregory', '2', 1),
+    ('Meza', 'Mauricio', '4', 2),
+    ('McBride', 'Jayla', '6', 3),
+    ('Applebaum', 'Laurence', '8', 4),
+    ('Atkins', 'Terrence', '8', 4);    
