bed75a37fc8eae819680631eb507074bc0fb01c7
JakMcCarthy <90810973+JakMcCarthy@users.noreply.github.com>
2022-09-10 17:37:00 -0400

    Create tables for department and role

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE department (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    name VARCHAR(30)
+);
+
+CREATE TABLE role (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(id) 
+    ON DELETE SET NULL
+);
